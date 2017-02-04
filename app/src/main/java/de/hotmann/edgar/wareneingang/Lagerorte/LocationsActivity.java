037d2502d60be93022281634bafd0beea45d8e62
edgar <edgarhotmann@gmail.com>
2017-02-05 00:53:07 +0100

    Lagerorte neuer Ansatz

--- app/src/main/java/de/hotmann/edgar/wareneingang/Lagerorte/LocationsActivity.java
+++ app/src/main/java/de/hotmann/edgar/wareneingang/Lagerorte/LocationsActivity.java
@@ -1,7 +1,9 @@
-package de.hotmann.edgar.wareneingang;
+package de.hotmann.edgar.wareneingang.Lagerorte;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,27 +13,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import de.hotmann.edgar.wareneingang.Eingang.WareneingangPaletten;
+import de.hotmann.edgar.wareneingang.R;
 
-
-public class MainstartActivity extends AppCompatActivity
+public class LocationsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainstart);
+        setContentView(R.layout.activity_locations);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        }); */
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -56,7 +57,7 @@ public class MainstartActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.mainstart, menu);
+        getMenuInflater().inflate(R.menu.locations, menu);
         return true;
     }
 
@@ -81,14 +82,13 @@ public class MainstartActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_palette) {
-            Intent palettenintent = new Intent(this, WareneingangPaletten.class);
-            this.startActivity(palettenintent);
-        } else if (id == R.id.nav_reihen) {
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_eingang) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_settings) {
+        } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
 
